afc005b69f930b984ea8a81008cf030d5c0f4f62
Simon-Raaber <95470326+SimonRaaber@users.noreply.github.com>
2023-06-16 08:25:53 +0200

    changed url in cors

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost",
+            policy.WithOrigins("http://localhost:4200",
                                 "https://student.cloud.htl-leonding.ac.at/s.raaber/onlinereservation/");
         });
 });
