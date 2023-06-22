6eb1a96e3071d05fca2890e3212f4822c88645e0
Basti <sebastian2005witzeneder@gmail.com>
2023-06-22 13:54:13 +0200

    Tests for employee and tables

--- Tischreservierung.Tests/PersonTest/Repository/EmployeeRepositoryTest.cs
+++ Tischreservierung.Tests/PersonTest/Repository/EmployeeRepositoryTest.cs
@@ -1,6 +1,60 @@
-﻿namespace Tischreservierung.Tests.PersonTest.Repository
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data.RestaurantRepo;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistence.Data.User;
+using Core.Models.User;
+
+namespace Tischreservierung.Tests.PersonTest.Repository
 {
     public class EmployeeRepositoryTest
     {
+        private readonly OnlineReservationContext _context;
+
+        public EmployeeRepositoryTest()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OnlineReservationContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            _context = new OnlineReservationContext(optionsBuilder.Options);
+        }
+
+        EmployeeRepository TestData()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() { EMail = "test1@gmail.com", RestaurantId = 1, FamilyName = "Mauer"},
+                new Employee() { EMail = "test2@gmail.com", RestaurantId = 2, FamilyName = "Bucek"},
+                new Employee() { EMail = "test3@gmail.com", RestaurantId = 2, FamilyName = "Luger"},
+                new Employee() { EMail = "test4@gmail.com", RestaurantId = 3, FamilyName = "Krikri"},
+                new Employee() { EMail = "test5@gmail.com", RestaurantId = 1, FamilyName = "Raa"},
+            };
+
+            _context.AddRange(employees);
+            _context.SaveChanges();
+
+            return new EmployeeRepository(_context);
+        }
+
+        [Fact]
+        public async Task GetByRestaurant()
+        {
+            var repository = TestData();
+            var result = await repository.GetByRestaurant(2);
+            Assert.NotNull(result);
+
+            var list = result.ToList();
+
+            Assert.Equal(2, list.Count);
+
+            Assert.Equal(2, list[0].RestaurantId);
+            Assert.Equal("test2@gmail.com", list[0].EMail);
+            Assert.Equal("test3@gmail.com", list[1].EMail);
+            Assert.Equal(2, list[1].RestaurantId);
+        }
     }
 }
