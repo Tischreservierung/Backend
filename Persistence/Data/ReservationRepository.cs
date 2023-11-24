4db91dc9ef844cab0dbc6f4a49136390b6d6179b
Simon-Raaber <95470326+SimonRaaber@users.noreply.github.com>
2023-11-24 10:43:29 +0100

    added restaurant service

--- Persistence/Data/ReservationRepository.cs
+++ Persistence/Data/ReservationRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Contracts;
+using Core.Dto;
 using Core.Models;
 using Microsoft.EntityFrameworkCore;
+using Core.Util;
 
 namespace Persistence.Data
 {
@@ -10,14 +12,36 @@ namespace Persistence.Data
         {
         }
 
-        public async Task<IEnumerable<Reservation>> GetByCustomer(int customerId)
+        public async Task<IEnumerable<ReservationDto>> GetByCustomer(int customerId)
         {
-            return await _dbSet.Where(r => r.CustomerId == customerId).ToListAsync();
+            return await _dbSet.Where(r => r.CustomerId == customerId).Select(x => new ReservationDto()
+            {
+                CustomerId = x.CustomerId,
+                RestaurantId = x.RestaurantId,
+                Day = x.ReservationDay,
+                StartTime = x.StartTime.ToDateTime(),
+                EndTime = x.EndTime.ToDateTime(),
+                RestaurantTableId = x.RestaurantTableId,
+                CustomerName = x.Customer!.Name,
+                RestaurantName = x.Restaurant!.Name,
+                Persons = x.RestaurantTable!.SeatPlaces
+            }).ToListAsync();
         }
 
-        public async Task<IEnumerable<Reservation>> GetByRestaurant(int restaurantId)
+        public async Task<IEnumerable<ReservationDto>> GetByRestaurant(int restaurantId)
         {
-            return await _dbSet.Where(r => r.RestaurantId == restaurantId).ToListAsync();
+            return await _dbSet.Where(r => r.RestaurantId == restaurantId).Select(x => new ReservationDto()
+            {
+                CustomerId = x.CustomerId,
+                RestaurantId = x.RestaurantId,
+                Day = x.ReservationDay,
+                StartTime = x.StartTime.ToDateTime(),
+                EndTime = x.EndTime.ToDateTime(),
+                RestaurantTableId = x.RestaurantTableId,
+                CustomerName = x.Customer!.Name,
+                RestaurantName = x.Restaurant!.Name,
+                Persons = x.RestaurantTable!.SeatPlaces
+            }).ToListAsync();
         }
 
         public async Task<IEnumerable<Reservation>> GetByRestaurantAndDay(int restaurantId, DateTime day)
